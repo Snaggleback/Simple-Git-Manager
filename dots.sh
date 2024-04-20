2f2d8dc16705916a6ce8c09892140e3793acf941
Ítalo <lixoliteral@gmail.com>
2024-04-20 19:35:40 -0300

    Recurso: Cria repositório caso não haja
    As configurações foram atualizadas para atender melhor às minhas necessidades. Veja os arquivos de configuração em questão: dots.sh (tipo: arquivo, foi modificado),  plugins/asker-yes-no.sh (tipo: arquivo, foi modificado),  plugins/asker.sh (tipo: arquivo, foi modificado),  plugins/clone-files.sh (tipo: arquivo, foi modificado),  plugins/press-close.sh (tipo: arquivo, foi modificado),  config/ (tipo: diretório, foi adicionado),

--- dots.sh
+++ dots.sh
@@ -9,6 +9,28 @@ source plugins/press-close.sh
 source plugins/asker-yes-no.sh
 source plugins/asker.sh
 
+repository_git_path=$(jq -r ".repository_path" <"./config/config.json")
+
+mkdir -p "${repository_git_path}"
+cd "${repository_git_path}" || exit
+
+# Verifica se o diretório contém o diretório .git
+if [ -d ".git" ]; then
+    echo "O diretório ${repository_git_path} contém um repositório Git"
+else
+    asker_yes_no "O diretório ${repository_git_path} não contém um repositório Git. Deseja criar um?" "S" last_res
+    if [ "${last_res}" = true ]; then
+        git init
+        git branch -M main
+        asker "Deseja adicionar remotamente o repositório github? (tecle enter para não):" last_res
+        if [ -n "${last_res}" ]; then
+            git remote add origin "${last_res}"
+        fi
+    else # Se não deseja criar, encerra o script
+        exit 0
+    fi
+fi
+
 # Cria um array vazio para armazenar os arquivos modificados
 dotfiles_modified=()
 # Mostra o status dos arquivos modificados e atribui os arquivos modificados ao array
@@ -26,13 +48,16 @@ asker_yes_no "Deseja publicar essas alterações no github?" "S" last_res
 
 # Se o usuário deseja publicar
 if [ "${last_res}" = true ]; then
-     asker "Qual título você deseja adicionar? (tecle enter para usar o título padrão):" commit_title
-     asker "Qual descricão você deseja adicionar? (tecle enter para usar a descricão padrão):" commit_description
+    asker "Qual título você deseja adicionar? (tecle enter para usar o título padrão):" commit_title
+    asker "Qual descricão você deseja adicionar? (tecle enter para usar a descricão padrão):" commit_description
 
     asker_yes_no "Confirma a publicação com o título \"${commit_title}\" e a descricão \"${commit_description:0:20}...\"?" "S" last_res
 
     if [ "${last_res}" = true ]; then
         git add -A
         git commit -m "${commit_title}" -m "${commit_description}"
+        git push
     fi
 fi
+
+press_close
--- plugins/clone-files.sh
+++ plugins/clone-files.sh
@@ -1,1 +1,52 @@
 #!/bin/bash
+
+repository_path="${1}"
+
+# Lista dos caminhos que estão sendo monitorados atualmente
+monitored_path=(
+    # Arquivos de configuração dentro de ".config"
+    "$HOME/.config/dunst"
+    "$HOME/.config/htop"
+    "$HOME/.config/i3/config"
+    "$HOME/.config/kitty"
+    "$HOME/.config/picom"
+    "$HOME/.config/ranger"
+    "$HOME/.config/rofi"
+    "$HOME/.config/user-dirs.dirs"
+    "$HOME/.config/neofetch"
+    "$HOME/.config/fontconfig"
+    # Ícones svg/png do sistema personalizados
+    "$HOME/.icons/system"
+    # Scripts personalizados
+    "$HOME/.scripts"
+    # Aliases e personalizações do bash
+    "$HOME/.bashrc"
+    # Configurações do meu Visual Studio Code
+    "$HOME/.config/Code/User/settings.json"
+    "$HOME/.config/Code/User/keybindings.json"
+)
+
+# Copiando todos os arquivos para dentro da nossa pasta
+for path in "${monitored_path[@]}"; do
+
+    # Nome do caminho (sem contar o final) do diretório atual
+    directory_name=$(dirname "${path}")
+
+    # Lógica, se um caminho for igual à HOME do usuário, então significa que ele não é um diretório e sim um arquivo, ou seja, podemos simplesmente copia-lo
+    if [ "${directory_name}" = "$HOME" ]; then
+
+        # Copia então o caminho para o diretório atual
+        cp -rp "${path}" "${repository_path}"
+
+        # Pula para o próximo
+        continue
+    fi
+
+    # Imprime a base do diretório, sem contar o final e sem contar o $HOME
+    directory_base=$(echo "${directory_name}" | cut -d'/' -f4-)
+    # Se caso for diretório dentro de diretório, ele irá criar logo os diretórios
+    mkdir -p "${repository_path}/${directory_base}"
+
+    # Copia efetivamente do caminho orginal pata o nosso diretório (agora sem preocupações :D)
+    cp -rp "${path}" "${repository_path}/${directory_base}"
+done
