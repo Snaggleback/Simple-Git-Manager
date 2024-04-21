88987b0733369841c03194b5d2dd86d5c8b4c8a7
Ítalo <lixoliteral@gmail.com>
2024-04-20 23:10:34 -0300

    Refatoração: Código mais legível e refatorado
    Pequenas modificações para tornar o código menos repetitivo e mais legível

--- plugins/show-status.sh
+++ plugins/show-status.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# Função (usa git) para mostrar as alterações
+# Função para exibir o status das alterações
 show_status() {
     # Função para substituir os códigos de status por palavras personalizadas
     replace_status() {
-        local status
-        status="$1"
+        local status="$1"
         case "$status" in
-        M) echo "Modificado" ;;
-        \?\?) echo "Adicionado" ;;
-        A) echo "Adicionado" ;;
-        D) echo "Excluído" ;;
-        # Se não corresponder a nenhum dos casos acima, apenas imprime o status original
-        *) echo "$status" ;;
+            M) echo "Modificado" ;;
+            ??|A) echo "Adicionado" ;;
+            D) echo "Excluído" ;;
+            *) echo "$status" ;;
         esac
     }
 
@@ -29,30 +26,21 @@ show_status() {
     printf "Arquivos de configuração atualizados, veja as modificações:\n\n"
 
     # Loop utilizando o comando read
-    while read -r line; do
+    while IFS= read -r line; do
         # Divide a linha em dois campos (status e caminho do arquivo)
-        local status
-        status=$(echo "$line" | awk '{print $1}')
-        local file
-        file=$(echo "$line" | awk '{print $2}')
+        local status file
+        read -r status file <<<"$line"
 
-        # Cria uma variável local para armazenar o tipo
-        local type
+        local type named_status
+        # Determina o tipo (arquivo ou diretório)
         type="Tipo: "
+        [[ "${file}" == */ ]] && type+="diretório" || type+="arquivo"
 
         # Substitui o status usando a função replace_status
-        local named_status
         named_status=$(replace_status "${status}")
+        # Imprime o resultado
+        echo "- ${file} (${type}) (${named_status})"
 
-        # Verifica se o caminho do arquivo termina em uma barra
-        if [[ "${file}" == */ ]]; then
-            type+="diretório"
-        else # Se não termina em uma barra, é um arquivo
-            type+="arquivo"
-        fi
-
-        # Substitui o status usando a função replace_status e imprime o resultado
-        echo "- ${file} (${type})" "(${named_status})"
         # Armazena o caminho do arquivo e o status em um array passado pelo parâmetro da função
         eval "${1}+=(\"${file} (${type,,}, foi ${named_status,,}), \")"
     done <<<"$status_output" # Redireciona a saída para o loop while
